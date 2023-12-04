e5221a6270f35b647d07ef2ada50cb0672a5d509
Jarek Piórkowski <jarek@piorkowski.ca>
2023-12-04 00:16:51 -0500

    day 3, part 1 - incorrect so far

---
+++ day3/day3.py
@@ -0,0 +1,173 @@
+from collections import defaultdict
+from dataclasses import dataclass
+from typing import Any, Dict, List, Tuple
+
+
+SPACER = "."
+
+
+@dataclass(frozen=True)
+class Position:
+    y: int
+    x: int
+
+
+@dataclass
+class Result:
+    number: int
+    position: Position
+    end_x: int  # TODO also Position?
+
+
+def tokenize_line(line: str) -> List[str]:
+    results = []
+    line_length = len(line)
+
+    x = 0
+    while x < line_length:
+        char = line[x]
+        sequence = char
+
+        while char.isdigit():
+            char = line[x + 1] if x < (line_length - 1) else ""
+
+            if char.isdigit():
+                sequence += char
+                x += 1
+
+        results.append(sequence or char)
+
+        x += 1
+
+    return results
+
+
+def build_map_near_symbols(
+    symbols: Dict[Position, str], max_x: int, max_y: int
+) -> Dict[Position, List[Position]]:
+    result: Dict[Position, List[Position]] = defaultdict(list)
+
+    for orig in symbols.keys():
+        up = orig.y - 1
+        down = orig.y + 1
+        can_go_up = up >= 0
+        can_go_down = down <= max_y
+        left = orig.x - 1
+        right = orig.x + 1
+        can_go_left = left >= 0
+        can_go_right = right <= max_x
+
+        if can_go_up:
+            result[Position(x=orig.x, y=up)].append(orig)
+            if can_go_left:
+                result[Position(x=left, y=up)].append(orig)
+            if can_go_right:
+                result[Position(x=right, y=up)].append(orig)
+
+        if can_go_down:
+            result[Position(x=orig.x, y=down)].append(orig)
+            if can_go_left:
+                result[Position(x=left, y=down)].append(orig)
+            if can_go_right:
+                result[Position(x=right, y=down)].append(orig)
+
+        if can_go_left:
+            result[Position(x=left, y=orig.y)].append(orig)
+            if can_go_up:
+                result[Position(x=left, y=up)].append(orig)
+            if can_go_down:
+                result[Position(x=left, y=down)].append(orig)
+
+        if can_go_right:
+            result[Position(x=right, y=orig.y)].append(orig)
+            if can_go_up:
+                result[Position(x=right, y=up)].append(orig)
+            if can_go_down:
+                result[Position(x=right, y=down)].append(orig)
+
+    return result
+
+
+def find_numbers_and_symbols(
+    input: List[str],
+) -> Tuple[List[Result], Dict[Position, str]]:
+    results = []
+    symbols = {}
+    for y, line in enumerate(input):
+        line_parts = tokenize_line(line)
+        # line_parts now looks like ['7', '.', '.', '$', '.', '*', '.', '.', '.', '.']
+        # given input "7..$.*...."
+
+        x = 0
+        for part in line_parts:
+            if part.isnumeric():
+                part_length = len(part)
+                end_x = x + part_length - 1
+                # minus 1 to get "start position" of the last character in part
+
+                results.append(
+                    Result(number=int(part), position=Position(x=x, y=y), end_x=end_x)
+                )
+                x += part_length - 1  # skip to the end of the part
+            elif part != SPACER:
+                # save the fact that there's a symbol here
+                # assumes any other symbol is single-character, otherwise we'd do `x += part_length` here too
+                symbols[Position(x=x, y=y)] = part
+
+            x += 1
+
+    return results, symbols
+
+
+def is_position_near_symbol(result: Result, symbols: Dict[Position, Any]) -> bool:
+    for x in range(result.position.x, result.end_x + 1):
+        if Position(x=x, y=result.position.y) in symbols:
+            return True
+
+    return False
+
+
+def add_up_part_numbers(input: List[str]) -> int:
+    numbers, symbols = find_numbers_and_symbols(input)
+
+    map_near_symbols = build_map_near_symbols(
+        symbols, max_x=len(input[0]) - 1, max_y=len(input) - 1
+    )
+
+    numbers_near_symbols = [
+        number
+        for number in numbers
+        if is_position_near_symbol(number, map_near_symbols)
+    ]
+
+    return sum(n.number for n in numbers_near_symbols)
+
+
+test_data = [
+    "467..114..",
+    "...*......",
+    "..35..633.",
+    "......#...",
+    "610*7.....",  # note: changed to highlight a bug - was `617*......`
+    ".....+.58.",
+    "..592.....",
+    "......755.",
+    "7..$.*....",  # note: changed from test input, but doesn't change result
+    ".664.598..",
+    "..........",
+    "./663.273.",  # note: added from scratch to highlight another edge case
+]
+
+
+def task():
+    with open("input", "r") as f:
+        data = f.readlines()
+    output = add_up_part_numbers(data)
+    # 536688 reported as too high
+    print(output < 536688, output)
+
+
+if __name__ == "__main__":
+    output = add_up_part_numbers(test_data)
+    print(output == 4361 + 663, output)
+    task()
